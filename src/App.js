77e39eb31eeef4c9b9e6d19760054e8959405d8f
Teryisam <101949970+Teryisam@users.noreply.github.com>
2025-08-13 10:08:13 +0100

    Update App.js
    Added Grafana Faro Route monitoring

--- src/App.js
+++ src/App.js
@@ -1,40 +1,41 @@
 import "./App.css";
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import AboutView from "./components/AboutView";
-import SearchView from './components/SearchView';
-import MovieView from './components/MovieView';
+import SearchView from "./components/SearchView";
+import MovieView from "./components/MovieView";
 import { Switch, Route } from "react-router-dom";
+import { FaroRoute } from "@grafana/faro-react";
 
 function App() {
-
   const [searchResults, setSearchResults] = useState([]);
-  const [searchText, setSearchText] = useState('');
+  const [searchText, setSearchText] = useState("");
 
   useEffect(() => {
-    if(searchText) {
-      fetch(`https://api.themoviedb.org/3/search/movie?api_key=ab166ff82684910ae3565621aea04d62&language=en-US&query=${searchText}&page=1&include_adult=false`)
-        .then(response => response.json())
-        .then(data => {
-          setSearchResults(data.results)
-        })
+    if (searchText) {
+      fetch(
+        `https://api.themoviedb.org/3/search/movie?api_key=ab166ff82684910ae3565621aea04d62&language=en-US&query=${searchText}&page=1&include_adult=false`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          setSearchResults(data.results);
+        });
     }
-  }, [searchText])
-
+  }, [searchText]);
 
   return (
     <div>
       <Navbar searchText={searchText} setSearchText={setSearchText} />
       <Switch>
-        <Route path="/" exact>
+        <FaroRoute path="/" exact>
           <Home />
-        </Route>
-        <Route path="/about" component={AboutView} />
-        <Route path="/search">
+        </FaroRoute>
+        <FaroRoute path="/about" component={AboutView} />
+        <FaroRoute path="/search">
           <SearchView keyword={searchText} searchResults={searchResults} />
-        </Route>
-        <Route path="/movies/:id" component={MovieView} />
+        </FaroRoute>
+        <FaroRoute path="/movies/:id" component={MovieView} />
       </Switch>
     </div>
   );
