fd33d9b0833267001d58a419708a57c200d62894
Teryisam <101949970+Teryisam@users.noreply.github.com>
2025-08-13 10:09:28 +0100

    Update index.js
    Added Grafana Faro frontend monitoring collection and ors.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // <— this enables the toggler/collapse behavior
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+// import {
+//   BrowserRouter
+// } from "react-router-dom";
+
+import { createBrowserHistory } from "history";
+import { Router, Route } from "react-router-dom";
+import {
+  createReactRouterV5Options,
+  getWebInstrumentations,
+  initializeFaro,
+  ReactIntegration,
+  ReactRouterVersion,
+} from "@grafana/faro-react";
+import { TracingInstrumentation } from "@grafana/faro-web-tracing";
+
+const history = createBrowserHistory();
+
+initializeFaro({
+  url: "https://faro-collector-prod-eu-west-2.grafana.net/collect/66e57d2a236c4ccf4584468cd04657eb",
+  app: {
+    name: "Movie Search",
+    version: "1.0.0",
+    environment: "production",
+  },
+
+  instrumentations: [
+    // Mandatory, omits default instrumentations otherwise.
+    ...getWebInstrumentations(),
+
+    // Tracing package to get end-to-end visibility for HTTP requests.
+    new TracingInstrumentation(),
+
+    // React integration for React applications.
+    new ReactIntegration({
+      router: createReactRouterV5Options({
+        history, // the history object used by react-router
+        Route, // Route component imported from react-router package
+      }),
+    }),
+  ],
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router history={history}>
       <App />
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
